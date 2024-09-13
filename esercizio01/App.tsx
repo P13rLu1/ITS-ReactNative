af949d436de8fe17a7c25d0b819cc3b1a42ee9e5
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-09-13 11:22:44 +0200

    done first exercise

--- esercizio01/App.tsx
+++ esercizio01/App.tsx
@@ -1,117 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  ActivityIndicator,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+interface SalutoProps {
+  name?: string;
+}
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function Saluto(props: SalutoProps) {
+  const {name} = props;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [showName, setShowName] = useState<boolean>(true);
+
+  const toggleShowName = () => {
+    setShowName(!showName);
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+    <View style={styles.salutoContainer}>
+      <TouchableOpacity style={styles.button} onPress={toggleShowName}>
+        <Text style={styles.buttonText}>
+          {showName ? 'Hide name' : 'Show Name'}
+        </Text>
+      </TouchableOpacity>
+      <Text style={styles.salutoText}>
+        Ciao {showName && (name || 'React Native')}!
       </Text>
     </View>
   );
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Primo esercizio</Text>
+      <ActivityIndicator size="large" color="#0000ff" style={styles.loader} />
+      <Saluto name={'Francesco'} />
+      <Saluto name={'Gian Franco'} />
+      <Saluto />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f8ff',
   },
-  sectionTitle: {
+  headerText: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#ff6347', // Rosso pomodoro
   },
-  sectionDescription: {
-    marginTop: 8,
+  loader: {
+    marginVertical: 20,
+  },
+  salutoContainer: {
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#add8e6', // Azzurro chiaro
+    alignItems: 'center',
+  },
+  salutoText: {
     fontSize: 18,
-    fontWeight: '400',
+    color: '#4b0082', // Indaco
+    fontWeight: '600',
+    marginTop: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: '#ff6347', // Rosso pomodoro
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff', // Bianco
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
