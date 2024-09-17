de40fcfe5c3df1c2b39969017687f2f4b196ce90
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-09-17 10:03:49 +0200

    added new exercise

---
+++ papadopolApp/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView, Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView >
+      <Text>Daje Roma Daje</Text>
+      <Text>Papadopole</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
